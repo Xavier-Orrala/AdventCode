c3c6d47fa0f08ca3d202f3986d3f27d69f5098ca
xavier <xavier@LAPTOP-F9ARRU66>
2023-12-01 13:22:18 -0500

    Test with Python

---
+++ AdventCode/Day1.py
@@ -0,0 +1,7 @@
+def getValue(string combo):
+    '''
+    Will obtain the first and last number of the hidden combination
+    and add it to the overall code needed by the Elves
+    '''
+    for num in combo:
+
