14bccfdfbf4d894dde4230b97b33a8e022bba3f3
xavier <xavier@LAPTOP-F9ARRU66>
2023-12-01 23:16:07 -0500

    Part 1 + Part 2 of Day 1 Finished

--- AdventCode/Day1.py
+++ AdventCode/Day1.py
@@ -3,6 +3,7 @@ def getValue():
     Will obtain the first and last number of the hidden combination
     and add it to the overall code needed by the Elves
     '''
+    key = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     combo = open('Day1ElfCode.txt', 'r')
     code = combo.readlines()
     total = 0
@@ -10,9 +11,20 @@ def getValue():
     second = ""
     for line in code:
        values = ""
+       test = ""
+       x = -1
        for num in line:
+           x += 1
            if num.isdigit() == True:
                  values = values + num
+                 #print("h",num)
+                 # For loop, goes through key array, checks if there are keywords in the num array
+           else:
+               for i in range(0,len(key)):
+                   length = int(len(key[i]))
+                   if(line[x:length+x] == key[i]):
+                       values = values + str(i)
+                       #print("x",num)
        if len(values) > 2:
            first,second = values[0], values[-1]
            values = first + second
